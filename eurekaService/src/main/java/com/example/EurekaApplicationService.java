aea1dc2a4b44bffb6e25b9e50f5c9ed79c674cc2
luqing870819 <303375917@qq.com>
2019-06-03 18:07:44 +0800

    测试提交

--- eurekaService/src/main/java/com/example/EurekaApplicationService.java
+++ eurekaService/src/main/java/com/example/EurekaApplicationService.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//项目启动类
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplicationService {
